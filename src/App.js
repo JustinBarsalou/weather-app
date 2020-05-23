82266e189efdb98029e52260790f984037bbb5c9
JustinBarsalou <justin.barsalou@g.austincc.edu>
2020-05-23 12:19:36 -0500

    added contents of main app, including the date, and placeholders for type of weather and degrees

--- src/App.js
+++ src/App.js
@@ -6,11 +6,37 @@ const api = {
 }
 
 function App() {
+  
+  const createDate = (d) => {
+
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    let result = `${day} ${date} ${month} ${year}`;
+
+    return result;
+  }
+
   return (
     <div className="App app-background">
     <main>
+
       <div className='search-box'>
-      <input type='text' className='search-bar' placeholder='Search'/>
+        <input type='text' className='search-bar' placeholder='Search'/>
+      </div>
+      
+      <div className="location-box">
+        <div className='location'>Location</div>
+        <div className='date'>{createDate(new Date())}</div>
+      </div>
+
+      <div className='weather-box'>
+        <div className='temperature'>75º</div>
+        <div className='weather-type'>Sunny</div>
 
       </div>
 
