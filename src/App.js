64b9ae7ae0439862abb8585e1a2c50c8229991ab
JustinBarsalou <justin.barsalou@g.austincc.edu>
2020-05-23 11:55:41 -0500

    got background and search bar in

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const api = {
+  key: "2e967a06c90ff16ac5be7261e5f206db",
+  baseURL: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App app-background">
+    <main>
+      <div className='search-box'>
+      <input type='text' className='search-bar' placeholder='Search'/>
+
+      </div>
+
+    </main>
     </div>
   );
-  
+
 }
 
 export default App;
